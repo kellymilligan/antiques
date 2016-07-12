f8b7131fe9ef86f2d0ad4f95f6a46d1f4ad081e7
Kelly Milligan <kelly@milligan.net.nz>
2016-07-12 17:24:45 +0200

    Create key_code_map.js

---
+++ key_code_map.js
@@ -0,0 +1,34 @@
+define( function () {
+
+    'use strict';
+
+    return [
+
+        {
+            code: 27,
+            name: 'ESC'
+        },
+        {
+            code: 9,
+            name: 'TAB'
+        },
+        {
+            code: 16,
+            name: 'SHIFT'
+        },
+        {
+            code: 32,
+            name: 'SPACEBAR'
+        },
+        {
+            code: 13,
+            name: 'ENTER'
+        },
+        {
+            code: 8,
+            name: 'BACKSPACE'
+        }
+
+    ];
+
+});
